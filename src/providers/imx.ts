ec898fb8842d17c768cec1bbb41d5fc56a94520b
Gerard 't Hooft <gerardthooft@tuta.io>
2026-06-23 17:44:51 +0200

    fix: generalize imx pattern

--- src/providers/imx.ts
+++ src/providers/imx.ts
@@ -2,14 +2,14 @@ import type { ImageProvider } from "../types";
 import { lazy } from "../utils/lazy";
 
 /**
- * Example: https://image.imx.to/u/t/yyyy/mm/dd/xxx.jpg -> https://image.imx.to/u/i/yyyy/mm/dd/xxx.jpg
+ * Example: https://imx.to/{a}/{b}/yyyy/mm/dd/xxx.jpg -> https://image.imx.to/u/i/yyyy/mm/dd/xxx.jpg
  */
 export const mapImxUrl: ImageProvider = async (url: string) => {
   if (!url.includes("imx.to")) {
     return { type: "skip" };
   }
 
-  const regex = /https:\/\/(?:image\.)?imx\.to\/u\/t\/(.*?)\.(jpe?g)/;
+  const regex = /https:\/\/(?:image\.|s\d+\.)?imx\.to\/[a-z]+\/[a-z]+\/(.*?)\.(jpe?g)/;
   const match = url.match(regex);
   if (!match) {
     return {
@@ -18,7 +18,7 @@ export const mapImxUrl: ImageProvider = async (url: string) => {
         provider: "imx",
         url,
         reason:
-          "URL format does not match expected pattern: https://[image.]imx.to/u/t/{path}.(jpg|jpeg)",
+          "URL format does not match expected pattern: https://[image.]imx.to/{a}/{b}/{path}.(jpg|jpeg)",
       },
     };
   }
